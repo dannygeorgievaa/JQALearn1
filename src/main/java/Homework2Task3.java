3769e8f2b0c96003fd92c039d85577bc2c438547
dannygeorgievaa <dannygeorgievaa@gmail.com>
2022-11-11 01:02:18 +0200

    Initial commit

--- src/main/java/Homework2Task3.java
+++ src/main/java/Homework2Task3.java
@@ -1,6 +1,9 @@
 public class Homework2Task3 {
     public static void main(String[] args) {
         double [] myArray = new double[] {13.6, 1.7, 1.5, 7.9, 77.9};
-        System.out.println(myArray);
+        System.out.println("Actual values: ");
+        for (double value : myArray) {
+            System.out.println("Value = " + value);
+        }
     }
 }
--- src/main/java/Homework2Task5.java
+++ src/main/java/Homework2Task5.java
@@ -1,9 +1,17 @@
+import static java.lang.System.arraycopy;
+
 public class Homework2Task5 {
     public static void main(String[] args) {
-            int arrayOne[] = {10, 12, 1, 8, 4};
-            int arrayTwo = arrayOne.length;
-         //   arrayTwo = arrayOne;
-            System.out.println(arrayTwo);
+        int arrayOne[] = { 10, 12, 1, 8, 4 };
+        int arrayTwo[] = new int[arrayOne.length];
+        arrayTwo = arrayOne;
+        System.out.println(" Array one has: ");
+        for (int i = 0; i < arrayTwo.length; i++)
+            System.out.print(arrayOne[i] + " ");
+
+            System.out.println("\n\n Array two has: ");
+        for (int i = 0; i < arrayTwo.length; i++)
+            System.out.print(arrayTwo[i] + " ");
         }
 }
 
