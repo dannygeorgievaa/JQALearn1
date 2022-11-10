d8170d232719dce1f2880ae7e29153adf10e1063
dannygeorgievaa <dannygeorgievaa@gmail.com>
2022-11-11 00:48:42 +0200

    Initial commit

--- src/main/java/Homework2Task6.java
+++ src/main/java/Homework2Task6.java
@@ -1,14 +1,15 @@
+import java.util.Arrays;
+
 public class Homework2Task6 {
     public static void main(String[] args) {
         int array1 [] = { 1, 2, 3 };
         int array2 [] = { 1, 2, 3 };
 
-        // Comparing arrays if equal or not
-        // using == operator
-        if (array1 == array2)
+       if   ( Arrays.equals(array1,array2))
 
+        {
             System.out.println("Same");
-
+        }
         else
         System.out.println("Not same");
     }
