82bfdeafd8738cbc7222661997a04c2369f22dc6
dannygeorgievaa <dannygeorgievaa@gmail.com>
2023-01-28 08:52:13 +0200

    Homework 6

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class helloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
@@ -6,7 +9,10 @@ public class helloWorld {
         String lastName = "Mihaylova";
         System.out.println(firstName + " "+ lastName);
 
-
+        System.setProperty("webdriver.chrome.driver","/Users/dannygeorgievaa/Downloads/chromedriver_mac64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:www.google.com");
+        driver.quit();
     }
 }
 
