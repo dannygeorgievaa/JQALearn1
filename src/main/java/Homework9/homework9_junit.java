7bbe39ae20690c2b9e9259b43a8fd9059095d6fb
dannygeorgievaa <dannygeorgievaa@gmail.com>
2023-02-06 16:07:13 +0200

    new

--- src/main/java/Homework9/homework9_junit.java
+++ src/main/java/Homework9/homework9_junit.java
@@ -1,15 +1,29 @@
-package Homework8;
-
+package Homework9;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.*;
+public class homework9_junit {
 
-public class FirstTestCase {
-    public static void main(String[] args) {
+    private static ChromeDriver driver;
+   // WebElement element;
+
+    @BeforeClass
+    public static void openBrowser(){
+        driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver","/Users/dannygeorgievaa/Downloads/chromedriver_mac64/chromedriver");
-        WebDriver driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void openPage () {
+
+        System.out.println("Starting test " + new Object() {
+        }.getClass().getEnclosingMethod().getName());
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("L2AGLb")).click();
@@ -17,7 +31,13 @@ public class FirstTestCase {
         searchBox.sendKeys("test 123");
         WebElement searchButton = driver.findElement(By.className("gNO89b"));
         searchButton.click();
+    }
 
+            @AfterClass
+        public static void closeBrowser(){
+            driver.quit();
 
     }
+
 }
+
