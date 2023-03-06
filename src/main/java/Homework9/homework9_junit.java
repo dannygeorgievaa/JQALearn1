bd760383715fc0e3e4967595497ce3552503514c
dannygeorgievaa <dannygeorgievaa@gmail.com>
2023-03-06 11:02:51 +0200

    homework10

--- src/main/java/Homework9/homework9_junit.java
+++ src/main/java/Homework9/homework9_junit.java
@@ -7,31 +7,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
 public class homework9_junit {
 
-    private static ChromeDriver driver;
-   // WebElement element;
+    public static ChromeDriver driver;
 
-    @BeforeClass
-    public static void openBrowser(){
-        driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver","/Users/dannygeorgievaa/Downloads/chromedriver_mac64/chromedriver");
-
+    {
+        // WebElement element;
+        System.setProperty("webdriver.chrome.driver", "/Users/dannygeorgievaa/Downloads/chromedriver_mac64/chromedriver");
     }
 
-    @Test
-    public void openPage () {
-
+    @BeforeClass
+    public static void openBrowser() {
+         driver = new ChromeDriver();
         System.out.println("Starting test " + new Object() {
         }.getClass().getEnclosingMethod().getName());
         driver.get("https://www.google.com/");
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals("https://www.google.com/",actualUrl);
         driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openPage() {
+
+
         driver.findElement(By.id("L2AGLb")).click();
         WebElement searchBox = driver.findElement(By.className("gLFyf"));
         searchBox.sendKeys("test 123");
         WebElement searchButton = driver.findElement(By.className("gNO89b"));
         searchButton.click();
+        WebDriver.Timeouts implicitlyWait = driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     }
+
 
     @AfterClass
         public static void closeBrowser(){
